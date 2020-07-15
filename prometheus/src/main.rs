ee19c870d5d8ddd6d54e8adb973a26cf9a1ae068
Eric Caleb <caleberic@tuta.io>
2020-07-15 19:38:44 +0200

    build up metrics for different paths

--- prometheus/src/main.rs
+++ prometheus/src/main.rs
@@ -1,37 +1,51 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
-// use rocket::routes;
+#[macro_use] extern crate prometheus;
+#[macro_use] extern crate lazy_static;
+
 use prometheus::{Opts, Registry, Counter, TextEncoder, Encoder};
+
+lazy_static! {
+    static ref PLAYGROUND_HTTP_REQUESTS: Counter = register_counter!(opts!(
+        "playground_http_requests_total",
+        "Total number of HTTP requests made to /playground.",
+        labels! {"handler" => "all",}
+    )).unwrap();
+    static ref MAIN_HTTP_REQUESTS: Counter = register_counter!(opts!(
+        "main_http_requests_total",
+        "Total number of HTTP requests made to /.",
+        labels! {"handler" => "all",}
+    )).unwrap();
+}
 
 #[get("/")]
 fn index() -> String {
-    // Create a Counter.
-    let counter_opts = Opts::new("test_counter", "test counter help");
-    let counter = Counter::with_opts(counter_opts).unwrap();
-
-    // Create a Registry and register Counter.
-    let r = Registry::new();
-    r.register(Box::new(counter.clone())).unwrap();
-
     // Inc.
-    counter.inc();
-    counter.inc();
+    MAIN_HTTP_REQUESTS.inc();
 
     // Gather the metrics.
     let mut buffer = vec![];
     let encoder = TextEncoder::new();
-    let metric_families = r.gather();
+    let metric_families = prometheus::gather();
     encoder.encode(&metric_families, &mut buffer).unwrap();
 
-    // Output to the standard output.
-    // println!("{}", String::from_utf8(buffer).unwrap());
     return String::from_utf8(buffer).unwrap()
+}
 
+#[get("/playground")] 
+fn playground() -> String {
 
+    let encoder = TextEncoder::new();
+
+    PLAYGROUND_HTTP_REQUESTS.inc();
+    let metric_families = prometheus::gather();
+    let mut buffer = vec![];
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+    return String::from_utf8(buffer).unwrap()
 }
 
 #[tokio::main]
 async fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, playground]).launch();
 }
