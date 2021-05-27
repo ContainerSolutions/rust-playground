93f977040cb50feadde3e4c622303007ba044895
Eric Caleb <caleberic@tuta.io>
2021-05-27 16:23:25 +0200

    cargo fmt

--- rq/src/handlers.rs
+++ rq/src/handlers.rs
@@ -1,16 +1,13 @@
-
-use prometheus::{TextEncoder, Encoder};
+use prometheus::{Encoder, TextEncoder};
 
 pub async fn metrics() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
-
     let encoder = TextEncoder::new();
-        
+
     let metric_families = prometheus::gather();
     let mut buffer = vec![];
     encoder.encode(&metric_families, &mut buffer).unwrap();
-    
-    let res = String::from_utf8(buffer).unwrap();
 
+    let res = String::from_utf8(buffer).unwrap();
 
     return Ok(Box::new(res));
 }
--- rq/src/main.rs
+++ rq/src/main.rs
@@ -1,7 +1,9 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate prometheus;
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate prometheus;
+#[macro_use]
+extern crate lazy_static;
 
 use prometheus::Counter;
 use warp::Filter;
@@ -13,44 +15,35 @@ lazy_static! {
         "playground_http_requests_total",
         "Total number of HTTP requests made to /playground.",
         labels! {"handler" => "all",}
-    )).unwrap();
+    ))
+    .unwrap();
     static ref MAIN_HTTP_REQUESTS: Counter = register_counter!(opts!(
         "main_http_requests_total",
         "Total number of HTTP requests made to /.",
         labels! {"handler" => "all",}
-    )).unwrap();
+    ))
+    .unwrap();
 }
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
-    let index =  warp::path::end()
-        .and(warp::get())
-        .map(|| {
-            MAIN_HTTP_REQUESTS.inc();
-            "rq: OK"
-        });
+    let index = warp::path::end().and(warp::get()).map(|| {
+        MAIN_HTTP_REQUESTS.inc();
+        "rq: OK"
+    });
 
-    let playground =  warp::path("playground")
-        .and(warp::get())
-        .map(|| {
+    let playground = warp::path("playground").and(warp::get()).map(|| {
         PLAYGROUND_HTTP_REQUESTS.inc();
         "rq: Playground OK"
     });
 
-    let metrics =  warp::path("metrics")
+    let metrics = warp::path("metrics")
         .and(warp::get())
         .and_then(handlers::metrics);
 
-    let routes = index
-        .or(metrics)
-        .or(playground);
+    let routes = index.or(metrics).or(playground);
 
-
-    warp::serve(routes)
-        .run(([127, 0, 0, 1], 8000))
-        .await;
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
-
-
