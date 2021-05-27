2ebd1f5790a710649fbd39bf7a718b2db2646d59
Eric Caleb <caleberic@tuta.io>
2021-05-27 18:40:42 +0200

    add playground handlers

--- rq/src/handlers.rs
+++ rq/src/handlers.rs
@@ -1,6 +1,14 @@
 use prometheus::{Encoder, TextEncoder};
 
+pub async fn index() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
+    info!("index");
+    crate::MAIN_HTTP_REQUESTS.inc();
+    let res = format!("rq: {}", crate::VERSION);
+    Ok(Box::new(warp::reply::json(&res)))
+}
+
 pub async fn metrics() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
+    info!("metrics");
     let encoder = TextEncoder::new();
 
     let metric_families = prometheus::gather();
@@ -8,6 +16,14 @@ pub async fn metrics() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
     encoder.encode(&metric_families, &mut buffer).unwrap();
 
     let res = String::from_utf8(buffer).unwrap();
+    info!("metrics: {}", res);
 
     return Ok(Box::new(res));
+}
+
+pub async fn playground() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
+    info!("playground");
+    crate::PLAYGROUND_HTTP_REQUESTS.inc();
+    let res = format!("playground:{}", crate::VERSION);
+    Ok(Box::new(warp::reply::json(&res)))
 }
