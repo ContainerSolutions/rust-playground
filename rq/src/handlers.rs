5d0b48e9e4817ada65e6921a116da9b8f6e65cb3
Eric Caleb <caleberic@tuta.io>
2021-05-27 16:13:57 +0200

    add warp handlers

---
+++ rq/src/handlers.rs
@@ -0,0 +1,17 @@
+
+use prometheus::{Counter, TextEncoder, Encoder};
+use warp::Filter;
+
+pub async fn metrics() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
+
+    let encoder = TextEncoder::new();
+        
+    let metric_families = prometheus::gather();
+    let mut buffer = vec![];
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+    
+    let res = String::from_utf8(buffer).unwrap();
+
+
+    return Ok(Box::new(res));
+}
