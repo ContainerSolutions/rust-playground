506d4bdcfd01bcb2172087c59015bbfe159c9cbe
Eric Caleb <caleberic@tuta.io>
2021-06-07 14:54:28 +0200

    add health, bad and track response code metrics

--- rq/src/handlers.rs
+++ rq/src/handlers.rs
@@ -1,10 +1,53 @@
 use prometheus::{Encoder, TextEncoder};
+use serde::Serialize;
+use warp::http::StatusCode;
+
+#[derive(Serialize)]
+struct Response {
+    code: u16,
+    message: String,
+}
+
+fn track_status_code(status_code: usize) {
+    match status_code {
+        500..=599 => crate::RESPONSE_CODES
+            .with_label_values(&[&status_code.to_string(), "500"])
+            .inc(),
+        400..=499 => crate::RESPONSE_CODES
+            .with_label_values(&[&status_code.to_string(), "400"])
+            .inc(),
+        300..=399 => crate::RESPONSE_CODES
+            .with_label_values(&[&status_code.to_string(), "300"])
+            .inc(),
+        200..=299 => crate::RESPONSE_CODES
+            .with_label_values(&[&status_code.to_string(), "200"])
+            .inc(),
+        100..=199 => crate::RESPONSE_CODES
+            .with_label_values(&[&status_code.to_string(), "100"])
+            .inc(),
+        _ => (),
+    };
+}
 
 pub async fn index() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
+    let code;
+    let message;
+
     info!("index");
-    crate::MAIN_HTTP_REQUESTS.inc();
-    let res = format!("rq: {}", crate::VERSION);
-    Ok(Box::new(warp::reply::json(&res)))
+    crate::INDEX_REQUESTS_TOTAL.inc();
+    crate::REQUESTS_TOTAL.inc();
+
+    message = format!("rq: {}", crate::VERSION);
+    code = StatusCode::OK;
+
+    let json = warp::reply::json(&Response {
+        code: code.as_u16(),
+        message: message.into(),
+    });
+
+    track_status_code(code.as_u16().into());
+
+    Ok(Box::new(warp::reply::with_status(json, code)))
 }
 
 pub async fn metrics() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
@@ -22,14 +65,84 @@ pub async fn metrics() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
 }
 
 pub async fn playground() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
+    let code;
+    let message;
+
     info!("playground");
-    crate::PLAYGROUND_HTTP_REQUESTS.inc();
-    let res = format!("playground:{}", crate::VERSION);
-    Ok(Box::new(warp::reply::json(&res)))
+    crate::PLAYGROUND_REQUESTS_TOTAL.inc();
+    crate::REQUESTS_TOTAL.inc();
+
+    message = format!("playground:{}", crate::VERSION);
+    code = StatusCode::OK;
+
+    let json = warp::reply::json(&Response {
+        code: code.as_u16(),
+        message: message.into(),
+    });
+
+    track_status_code(code.as_u16().into());
+
+    Ok(Box::new(warp::reply::with_status(json, code)))
 }
 
 pub async fn version() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
+    let code;
+    let message;
+
     info!("version");
-    let res = format!("{}", crate::VERSION);
-    Ok(Box::new(warp::reply::json(&res)))
+    crate::REQUESTS_TOTAL.inc();
+
+    message = format!("{}", crate::VERSION);
+    code = StatusCode::OK;
+
+    let json = warp::reply::json(&Response {
+        code: code.as_u16(),
+        message: message.into(),
+    });
+
+    track_status_code(code.as_u16().into());
+
+    Ok(Box::new(warp::reply::with_status(json, code)))
+}
+
+pub async fn health() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
+    let code;
+    let message;
+
+    info!("health");
+
+    crate::REQUESTS_TOTAL.inc();
+
+    message = format!("Ok: {}", crate::VERSION);
+    code = StatusCode::OK;
+
+    let json = warp::reply::json(&Response {
+        code: code.as_u16(),
+        message: message.into(),
+    });
+
+    track_status_code(code.as_u16().into());
+
+    Ok(Box::new(warp::reply::with_status(json, code)))
+}
+
+pub async fn bad() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
+    let code;
+    let message;
+
+    info!("bad");
+
+    crate::REQUESTS_TOTAL.inc();
+
+    message = format!("Bad: {}", crate::VERSION);
+    code = StatusCode::BAD_REQUEST;
+
+    let json = warp::reply::json(&Response {
+        code: code.as_u16(),
+        message: message.into(),
+    });
+
+    track_status_code(code.as_u16().into());
+
+    Ok(Box::new(warp::reply::with_status(json, code)))
 }
